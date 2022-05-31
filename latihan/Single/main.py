70140a1b4877ed0f8262ec9fb96fddc5c1a6e262
aliance22 <andrealiance33@gmail.com>
2022-05-31 13:32:56 +0700

    open

--- latihan/Single/main.py
+++ latihan/Single/main.py
@@ -2,10 +2,11 @@ from balok import Balok
 from balok_controller import BalokController
 from balok_view import BalokView
 
-balok = Balok(4)
+panjang = Balok(8, 9, 10)
+
 penghitung_balok = BalokController()
 penampil_balok = BalokView()
 
-penampil_balok.show_luas(balok, penghitung_balok)
-penampil_balok.show_keliling(balok, penghitung_balok)
-penampil_balok.show_volume(balok, penghitung_balok)
+penampil_balok.show_luas(panjang, penghitung_balok)
+penampil_balok.show_keliling(panjang, penghitung_balok)
+penampil_balok.show_volume(panjang, penghitung_balok)
