d9cf7137174d336d3986742c18e4c429c3e3f73b
firgiansyahim <firgiansyahiqbalm@gmail.com>
2022-05-31 11:57:29 +0700

    aku tampan

---
+++ latihan/balok_controller.py
@@ -0,0 +1,11 @@
+from balok import Balok
+class BalokController:
+    
+    def hitung_luas_pernukaan(self, balok: Balok) -> float:
+        return 2 * ( (balok.get_panjang() * balok.get_lebar()) + (balok.get_panjang() * balok.get_tinggi()) + (balok.get_lebar() * balok.get_tinggi()) )   
+    
+    def hitung_lebar(self, balok : Balok) -> float:
+        return 4 *( balok.get_panjang() + balok.get_lebar() + balok.get_tinggi())
+    
+    def hitung_volume(self, balok : Balok) -> float :
+        return balok.get_panjang() * balok.get_lebar() * balok.get_tinggi()
