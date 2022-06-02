9a12c22febbd10474a4297f0f9435982c1f85aa9
aliance22 <andrealiance33@gmail.com>
2022-06-03 00:33:23 +0700

    Andre

--- latihan/open/main.py
+++
@@ -1,7 +0,0 @@
-from karakter import Karakter
-from karakter_pengendara import KarakterPengendara
-from karakter_penembak import KarakterPenembak
-
-view = Karakter ("waladolin",80)
-view = KarakterPenembak ("amin",90)
-view = KarakterPengendara ("stepen",69)
