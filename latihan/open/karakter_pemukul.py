92f41d5812bcad02555c585a272a271b346a7901
aliance22 <andrealiance33@gmail.com>
2022-06-03 00:48:25 +0700

    Andre

--- latihan/open/karakter_pemukul.py
+++
@@ -1,9 +0,0 @@
-from karakter import Karakter 
-
-class KarakterPemukul(Karakter):
-    
-    def nama(self, karakter : Karakter) -> str:
-        return("nama", karakter.get_nama) 
-    
-    def power(self, karakter : Karakter) -> float:
-        return("memukul", karakter.get_power)
