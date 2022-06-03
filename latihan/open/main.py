c4f1f33288f53c7137b8e165b466706d5e12ccdb
aliance22 <andrealiance33@gmail.com>
2022-06-03 10:15:15 +0700

    Andre

--- latihan/open/main.py
+++ latihan/open/main.py
@@ -3,10 +3,10 @@ from karakter_pengendara import KarakterPengendara
 from karakter_penembak import KarakterPenembak
 
 karakter1 = KarakterPemukul ("waladolin",80)
-print(karakter1.menyerang(),"memukul")
+print(karakter1.get_nama(), karakter1.menyerang(),"memukul")
 
-karakter2 = KarakterPenembak ("amin",90)
-print(karakter2.menyerang(),"memukul")
+karakter2 = KarakterPenembak ("amin",90,)
+print(karakter2.get_nama(), karakter2.menyerang(), "menembak")
 
 karakter3 = KarakterPengendara ("stepen",69)
-print(karakter3.menyerang(),"run")
+print(karakter3.get_nama(), karakter3.menyerang(),"run")
